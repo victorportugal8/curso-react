8b0928fba9e526d91c74ec4b1a1fd86ca55e1ece
Victor Portugal <victorportugalnobrega@gmail.com>
2023-02-17 18:06:38 -0300

    Criando um segundo componente com envio de parâmetros

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,8 +1,16 @@
 import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
+
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 const el = document.getElementById('root')
 
-ReactDOM.render(<div><Primeiro></Primeiro></div>, el) //1P: a mensagem; 2P: elemento onde a mensagem será injetada
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro titulo="Situação do Aluno" aluno="Itadori Yuji" nota={6.8} />
+    </div>, 
+    el
+)//1P: a mensagem; 2P: elemento onde a mensagem será injetada
