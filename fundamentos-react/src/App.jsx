6e6be1243dedd665d7d07e06ad433a82a47a917f
Victor Portugal <victorportugalnobrega@gmail.com>
2023-02-20 15:28:01 -0300

    Estilizando o componente Card

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -12,16 +12,16 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
-            <Card titulo="#04 - Desafio Número Aleatório">
+            <Card titulo="#04 - Desafio Número Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60}/>
             </Card>
-            <Card titulo="#03 - Fragmento">
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
             </Card>
-            <Card titulo="#02 - Com Parâmetro">
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
                 <ComParametro titulo="Situação do Aluno" aluno="Itadori Yuji" nota={6.8} />
             </Card>
-            <Card titulo="#01 - Primeiro Componente">
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
                 <Primeiro></Primeiro>
             </Card>
         </div>
