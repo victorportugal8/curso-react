ff1b97b001442720ed8db59f20a2f5867961de93
Victor Portugal <victorportugalnobrega@gmail.com>
2023-02-21 11:10:37 -0300

    Componente com estado

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,10 +2,12 @@ import React from "react"
 
 export default props =>{
     const cb = props.quandoClicar
+    const gerarIdade = () => Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+    const gerarForca = () => Math.random() > 0.5
     return(
         <div>
             <div>Filho</div>
-            <button onClick={_ => cb('Isshin Kurosaki', 432, true)}>
+            <button onClick={_ => cb('Isshin Kurosaki', gerarIdade(), gerarForca())}>
                 Fornecer Informações
             </button>
         </div>
--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,13 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 import IndiretaFilho from "./IndiretaFilho"
 
 export default props =>{
+    const [nome, setNome] = useState('?') //retorna 1 array com 2pos: 1-> valor em si; 2-> fn usada para alterar o valor
+    const [idade, setIdade] = useState('?')
+    const [forte, setForte] = useState('?')
     function fornecerInformacoes(nome, idade, forte){
-        console.log(nome, idade, forte)
+        setNome(nome)
+        setIdade(idade)
+        setForte(forte)
     }
     return(
         <div>
-            <div>Pai</div>
+            <div>
+                <span>{nome} </span>
+                <span><strong>{idade}</strong> </span>
+                <span>{forte ? 'Verdadeiro' : 'Falso'}</span>
+            </div>
             <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
         </div>
     )
