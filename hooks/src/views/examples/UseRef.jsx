9d75dcaf0bc0e4a0a7de5505f2ca9f6248f40ce2
Victor Portugal <victorportugalnobrega@gmail.com>
2023-02-25 11:12:58 -0300

    Desafio Merge useRef

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,13 +2,26 @@ import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = function(str1, str2){
+    return [...str1].map((el, i) => `${el}${str2[i] || ""}`).join("")
+}
+
 const UseRef = (props) => {
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
     const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
+
     // count.current++ -> controla TODAS as renderizações do componente
     useEffect(function(){
         count.current++
+        myInput2.current.focus()
     }, [value1]) //só modifica o valor de current quando o value1 for alterado
+    useEffect(function(){
+        count.current++
+        myInput1.current.focus()
+    }, [value2])
     return (
         <div className="UseRef">
             <PageTitle
@@ -19,11 +32,15 @@ const UseRef = (props) => {
             <div className="center">
                 <div>
                     <span className="text">Valor: </span>
-                    <span className="text">{value1} [</span>
+                    <span className="text">{merge(value1, value2)} [</span>
                     <span className="text red">{count.current}</span>
                     <span className="text">]</span>
                 </div>
-                <input type="text" className="input" value={value1} onChange={e => setValue1(e.target.value)} />
+                <input type="text" className="input" value={value1} onChange={e => setValue1(e.target.value)} ref={myInput1} />
+            </div>
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <input type="text" className="input" value={value2} onChange={e => setValue2(e.target.value)} ref={myInput2} />
             </div>
         </div>
     )
