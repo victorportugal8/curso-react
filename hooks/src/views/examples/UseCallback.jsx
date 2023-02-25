95f36d216f2622ec0637cc7c804df65498a9a39c
Victor Portugal <victorportugalnobrega@gmail.com>
2023-02-25 20:01:55 -0300

    Exercício useCallback(React.memo)

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import UseCallbackButtons from './UseCallbackBtns'
 
 const UseCallback = (props) => {
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function(delta){
+        setCount(current => current + delta)
+    }, [setCount]) //desde que setCount não seja modificado(o que não acontecerá normalmente), o retorno será o mesmo, porque a fn só é criado uma única vez
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
+                subtitle="Retorna uma função memoizada!" />
+            <SectionTitle title="Exercício #01"/>
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallbackButtons inc={inc} />
+            </div>
         </div>
     )
 }
