96db88108ee757f931876b9fa7c2c222759d053f
Victor Portugal <victorportugalnobrega@gmail.com>
2023-03-04 18:56:44 -0300

    Pré-renderização na prática

--- next-js/fundamentos/src/pages/index.jsx
+++ next-js/fundamentos/src/pages/index.jsx
@@ -14,8 +14,9 @@ export default function Inicio(){
             <Navegador destino="/jsx" texto="JSX" cor="crimson" />
             <Navegador destino="/navegacao" texto="Navegação #01" cor="green" />
             <Navegador destino="/cliente/RJ/123" texto="Navegação #02" cor="blue" />
-            <Navegador destino="/estado" texto="Componente com Estadao" cor="pink" />
-            <Navegador destino="/integracao_1" texto="Integração com API #01" cor="yellow" />
+            <Navegador destino="/estado" texto="Componente com Estadao" cor="#A45B71" />
+            <Navegador destino="/integracao_1" texto="Integração com API #01" cor="#42A9A9" />
+            <Navegador destino="/estatico" texto="Conteúdo Estático" cor="#FA054A" />
         </div>
     )
 }
