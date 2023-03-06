78e35848140f27206e08070ec08c405158c732c5
Victor Portugal <victorportugalnobrega@gmail.com>
2023-03-06 18:25:47 -0300

    Criando o componente Botão

--- next-js/next-crud/src/pages/index.tsx
+++ next-js/next-crud/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from "@/components/Botao";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
 import Cliente from "@/core/Cliente";
@@ -25,6 +26,9 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+          <Botao className="mb-4" cor="green">Novo Cliente</Botao>
+        </div>
         <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}/>
       </Layout>
     </div>
--- next-js/next-crud/tailwind.config.js
+++ next-js/next-crud/tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
   "./src/components/**/*.{js,ts,jsx,tsx}",
   
   ],
+  safelist: [
+    /^bg-/,
+    /^to-/,
+    /^from-/,
+    'from-green-400',
+    'from-blue-400',
+    'from-gray-400',
+    'to-gray-700',
+    'to-blue-700',
+    'to-green-700',
+  ],
   
   theme: {
     extend: {},
@@ -15,4 +26,4 @@ module.exports = {
   
   plugins: [],
   
-  }
+}
