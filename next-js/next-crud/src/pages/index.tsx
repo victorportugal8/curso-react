2fcc23c46558947e65f1867316d7d6c1cdd9f663
Victor Portugal <victorportugalnobrega@gmail.com>
2023-03-06 11:06:43 -0300

    Criando os componentes Layout e Titulo

--- next-js/next-crud/src/pages/index.tsx
+++ next-js/next-crud/src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Layout from "@/components/Layout";
+
 export default function Home() {
   return(
-    <div></div>
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
+    </div>
   )
 }
