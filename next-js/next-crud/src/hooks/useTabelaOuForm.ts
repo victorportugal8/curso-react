e2c42e24550d7246af19cefe872cb75af2556b1a
Victor Portugal <victorportugalnobrega@gmail.com>
2023-03-09 12:39:19 -0300

    Refactoring dos hooks e finalizando a aplicação

---
+++ next-js/next-crud/src/hooks/useTabelaOuForm.ts
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function useTabelaOuForm(){
+    const [visivel, setVisivel] = useState<'tabela' | 'formulario'>('tabela')
+
+    const exibirTabela = () => setVisivel('tabela')
+    const exibirFormulario = () => setVisivel('formulario')
+
+    return{
+        formularioVisivel: visivel === 'formulario',
+        tabelaVisivel: visivel === 'tabela',
+        exibirTabela,
+        exibirFormulario
+    }
+}
--- next-js/next-crud/src/pages/index.tsx
+++ next-js/next-crud/src/pages/index.tsx
@@ -1,46 +1,20 @@
-import ColecaoCliente from "@/backend/db/ColecaoCliente";
 import Botao from "@/components/Botao";
 import Formulario from "@/components/Formulario";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/Tabela";
-import Cliente from "@/core/Cliente";
-import ClienteRepositorio from "@/core/ClienteRepositorio";
-import { useEffect, useState } from "react";
+import useClientes from "@/hooks/useClientes";
 
 export default function Home() {
-  const repo: ClienteRepositorio = new ColecaoCliente()
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [clientes, setClientes] = useState<Cliente[]>([])
-  const [visivel, setVisivel] = useState<'tabela' | 'formulario'>('tabela')
-
-  useEffect(obterTodos ,[])
-  
-  function obterTodos(){
-    repo.obterTodos().then(clientes =>{
-      setClientes(clientes)
-      setVisivel('tabela')
-    })
-  }
-
-  function clienteSelecionado(cliente: Cliente){
-    setCliente(cliente)
-    setVisivel('formulario')
-  }
-
-  async function clienteExcluido(cliente: Cliente){
-    await repo.excluir(cliente)
-    obterTodos()
-  }
-
-  async function salvarCliente(cliente: Cliente){
-    await repo.salvar(cliente)
-    obterTodos()
-  }
-
-  function novoCliente(cliente: Cliente){
-    setCliente(Cliente.vazio())
-    setVisivel('formulario')
-  }
+  const {
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecionarCliente,
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
 
   return(
     <div className={`
@@ -49,15 +23,15 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           <>
             <div className="flex justify-end">
               <Botao className="mb-4" onClick={novoCliente}>Novo Cliente</Botao>
             </div>
-            <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}/>
+            <Tabela clientes={clientes} clienteSelecionado={selecionarCliente} clienteExcluido={excluirCliente}/>
           </>
         ) : (
-          <Formulario cliente={cliente} clienteMudou={salvarCliente} cancelado={() => setVisivel('tabela')} />
+          <Formulario cliente={cliente} clienteMudou={salvarCliente} cancelado={exibirTabela} />
         )}
       </Layout>
     </div>
