54c345ba91771c1a8b7f818d6bae7127e2ee2e85
Victor Portugal <victorportugalnobrega@gmail.com>
2023-03-09 18:56:35 -0300

    Configurando o servidor com o Express

---
+++ todo-app/back-end/src/config/server.js
@@ -0,0 +1,11 @@
+const port = 3000
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`BACK-END is running on port ${port}.`)
+})
