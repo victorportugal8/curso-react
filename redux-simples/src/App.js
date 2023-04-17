eade2926278759d2f734ccdd94a68b82e5542ebf
Victor Portugal <victorportugalnobrega@gmail.com>
2023-04-17 18:30:42 -0300

    Componente Intervalo

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux Simples</h1>
       <div className='linha'>
-        <Card title="Card 1" red>X</Card>
-        <Card title="Card 2" green>X</Card>
+        <Intervalo></Intervalo>
       </div>
       <div className='linha'>
+        <Card title="Card 2" green>X</Card>
         <Card title="Card 3" blue>Y</Card>
         <Card title="Card 4" purple>Y</Card>
       </div>
---
+++ redux-simples/src/components/Intervalo.jsx
@@ -0,0 +1,20 @@
+import Card from './Card'
+import './Intervalo.css'
+import React from 'react'
+
+export default prosp => {
+    return(
+        <Card title="Intervalo de Números" red>
+            <div className="Intervalo">
+                <span>
+                    <strong>Mínimo:</strong>
+                    <input type="number" value={0} readOnly />
+                </span>
+                <span>
+                    <strong>Máximo:</strong>
+                    <input type="number" value={10} readOnly />
+                </span>
+            </div>
+        </Card>
+    )
+}
