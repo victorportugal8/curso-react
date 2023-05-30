1cfcdda86ff1b441e4d755a5e85848d8c0c90876
Victor Portugal <victorportugalnobrega@gmail.com>
2023-05-30 18:43:05 -0300

    Obtendo o sumário de pagamentos

--- aplicacao-ciclos-pagamentos/back-end/src/api/cicloPagamento/servicoCicloPagamento.js
+++ aplicacao-ciclos-pagamentos/back-end/src/api/cicloPagamento/servicoCicloPagamento.js
@@ -23,4 +23,20 @@ CicloPagamento.route('count', (req, res, next) =>{
     })
 })
 
+CicloPagamento.route('sumario', (req, res, next) =>{
+    CicloPagamento.aggregate([{
+        $project: {credito: {$sum: "$creditos.valor"}, debito: {$sum: "$debitos.valor"}}
+    }, {
+        $group: {_id: null, credito: {$sum: "$credito"}, debito: {$sum: "$debito"}}
+    }, {
+        $project: {_id: 0, credito: 1, debito: 1}
+    }], (error, result) =>{
+        if(error){
+            res.status(500).json({errors: [error]})
+        } else{
+            res.json(result[0] || {credito: 0, debito: 0})
+        }
+    })
+})
+
 module.exports = CicloPagamento
--- aplicacao-ciclos-pagamentos/back-end/src/config/database.js
+++ aplicacao-ciclos-pagamentos/back-end/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/ciclospagamentos')
+module.exports = mongoose.connect('mongodb://localhost/ciclospagamentos', {userNewUrlParser: true})
 
 mongoose.Error.messages.general.required = "O atributor '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
