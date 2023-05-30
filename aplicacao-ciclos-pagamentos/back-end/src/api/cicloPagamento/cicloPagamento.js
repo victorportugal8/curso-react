8ba0493679dcd48414e29d8ca249b666d1fa4955
Victor Portugal <victorportugalnobrega@gmail.com>
2023-05-30 17:36:57 -0300

    Mapeamento Objeto-Documento

---
+++ aplicacao-ciclos-pagamentos/back-end/src/api/cicloPagamento/cicloPagamento.js
@@ -0,0 +1,23 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+const esquemaCredito = new mongoose.Schema({
+    nome: {type: String, required: true},
+    valor: {type: Number, min: 0, required: true}
+})
+
+const esquemaDebito = new mongoose.Schema({
+    nome: {type: String, required: true},
+    valor: {type: Number, min: 0, required: true},
+    status: {type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+})
+
+const esquemaCicloPagamento = new mongoose.Schema({
+    nome: {type: String, required: true},
+    mes: {type: Number, min: 1, max: 12, required: true},
+    ano: {type: Number, min: 1970, max: 2100, required: true},
+    creditos: [esquemaCredito],
+    debitos: [esquemaDebito]
+})
+
+module.exports = restful.model('CicloPagamento', esquemaCicloPagamento)
