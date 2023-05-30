a505ff8a0258453f30ee5c5baf730e78882fa609
Victor Portugal <victorportugalnobrega@gmail.com>
2023-05-30 17:47:35 -0300

    Corrigindo o método GET

--- aplicacao-ciclos-pagamentos/back-end/src/api/cicloPagamento/servicoCicloPagamento.js
+++ aplicacao-ciclos-pagamentos/back-end/src/api/cicloPagamento/servicoCicloPagamento.js
@@ -3,4 +3,14 @@ const CicloPagamento = require('./cicloPagamento')
 CicloPagamento.methods(['get', 'post', 'put', 'delete'])
 CicloPagamento.updateOptions({new: true, runValidators: true})
 
+CicloPagamento.route('get', (req, res, next) =>{
+    CicloPagamento.find({}, (err, docs) =>{
+        if(!err){
+            res.json(docs)
+        } else{
+            res.status(500).json({errors: [error]})
+        }
+    })
+})
+
 module.exports = CicloPagamento
