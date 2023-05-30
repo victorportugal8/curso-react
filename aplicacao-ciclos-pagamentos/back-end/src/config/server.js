1989d4ff6f0f539894a6f25da427cbd208a121b7
Victor Portugal <victorportugalnobrega@gmail.com>
2023-05-30 19:54:41 -0300

    Middleware Query Parser Int

--- aplicacao-ciclos-pagamentos/back-end/src/config/server.js
+++ aplicacao-ciclos-pagamentos/back-end/src/config/server.js
@@ -1,12 +1,15 @@
 const port = 3003
+
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const permitirCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(permitirCors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`BACK-END está rodando na porta ${port}.`)
