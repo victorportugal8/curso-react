68b9ca322bb022bc78776dee565e38088cab7e6d
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-01 19:31:20 -0300

    Implementando as Caixas de Valores

--- aplicacao-ciclos-pagamentos/front-end/src/dashboard/dashboard.jsx
+++ aplicacao-ciclos-pagamentos/front-end/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, {Component} from "react"
 
 import ContentHeader from "../comum/template/contentHeader"
 import Content from "../comum/template/content"
+import ValueBox from "../comum/widget/valueBox"
+import Row from "../comum/layout/row"
 
 class Dashboard extends Component{
     render(){
@@ -9,7 +11,11 @@ class Dashboard extends Component{
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10,00' text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10,00' text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value='R$ 00,00' text='Valor Consolidado' />
+                    </Row>
                 </Content>
             </div>
         )
