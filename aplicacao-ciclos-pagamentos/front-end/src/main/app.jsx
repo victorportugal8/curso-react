99b872ed8e3eac59a93052fefd10145f519af134
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-01 18:03:58 -0300

    Componente Menu

--- aplicacao-ciclos-pagamentos/front-end/src/main/app.jsx
+++ aplicacao-ciclos-pagamentos/front-end/src/main/app.jsx
@@ -2,9 +2,11 @@ import '../comum/template/dependencias'
 import React from "react"
 
 import Header from '../comum/template/header'
+import Sidebar from '../comum/template/sidebar'
 
 export default props =>(
     <div className="wrapper">
         <Header />
+        <Sidebar />
     </div>
 )
