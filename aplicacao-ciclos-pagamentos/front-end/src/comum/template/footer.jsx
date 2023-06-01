bd7f0860f7c003a4b7c818e70ed33fd88e98b167
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-01 18:25:58 -0300

    Componente Footer

---
+++ aplicacao-ciclos-pagamentos/front-end/src/comum/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default props =>(
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2023
+            <a href="https://github.com/victorportugal8" target="_blank"> Victor Portugal</a>
+        </strong>
+    </footer>
+)
--- aplicacao-ciclos-pagamentos/front-end/src/main/app.jsx
+++ aplicacao-ciclos-pagamentos/front-end/src/main/app.jsx
@@ -3,10 +3,15 @@ import React from "react"
 
 import Header from '../comum/template/header'
 import Sidebar from '../comum/template/sidebar'
+import Footer from '../comum/template/footer'
 
 export default props =>(
     <div className="wrapper">
         <Header />
         <Sidebar />
+        <div className="content-wrapper">
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer />
     </div>
 )
