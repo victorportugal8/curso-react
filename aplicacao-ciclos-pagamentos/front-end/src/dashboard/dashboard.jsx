f8730d6db302c127efcfbba6d0973a5cb32544f6
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-01 18:51:44 -0300

    Componente de título de conteúdo

--- aplicacao-ciclos-pagamentos/front-end/src/dashboard/dashboard.jsx
+++ aplicacao-ciclos-pagamentos/front-end/src/dashboard/dashboard.jsx
@@ -1,7 +1,19 @@
-import React from "react"
+import React, {Component} from "react"
 
-export default props =>(
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+import ContentHeader from "../comum/template/contentHeader"
+import Content from "../comum/template/content"
+
+class Dashboard extends Component{
+    render(){
+        return(
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
+                <Content>
+                    Dashboard
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
