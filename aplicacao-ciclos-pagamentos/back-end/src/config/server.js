d1e16d97aa9db3d272def6d2a249c0f5d3b4a051
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-12 17:40:14 -0300

    Corrigindo erro do cors

--- aplicacao-ciclos-pagamentos/back-end/src/config/server.js
+++ aplicacao-ciclos-pagamentos/back-end/src/config/server.js
@@ -8,7 +8,7 @@ const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
-server.use(permitirCors)
+server.use(permitirCors())
 server.use(queryParser())
 
 server.listen(port, function(){
