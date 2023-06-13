ac423871af1e2c3a340a3aab0e5d90d936eeb12e
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-13 18:01:08 -0300

    Campos com o componente personalizado

--- aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/cicloPagamentoForm.jsx
+++ aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/cicloPagamentoForm.jsx
@@ -1,15 +1,17 @@
 import React, {Component} from "react"
 import { reduxForm, Field } from "redux-form"
 
+import labelAndInput from "../comum/form/labelAndInput"
+
 class CicloPagamentoForm extends Component{
     render(){
         const {handleSubmit} = this.props
         return(
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name='nome' component='input' />
-                    <Field name='mes' component='input' />
-                    <Field name='ano' component='input' />
+                    <Field name='nome' component={labelAndInput} label='Nome' cols='12 4' placeholder="Informe o nome" />
+                    <Field name='mes' component={labelAndInput} type="number" label='Mês' cols='12 4' placeholder="Informe o mês" />
+                    <Field name='ano' component={labelAndInput} type="number" label='Ano' cols='12 4' placeholder="Informe o ano" />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Submit</button>
