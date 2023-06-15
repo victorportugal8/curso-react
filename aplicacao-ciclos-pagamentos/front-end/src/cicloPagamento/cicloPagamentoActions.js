8e240638df0dfdbfbe177fda052206da9e73cdeb
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-15 18:40:33 -0300

    Implementando a função do botão cancelar

--- aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/cicloPagamentoActions.js
+++ aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/cicloPagamentoActions.js
@@ -5,6 +5,7 @@ import {reset as resetForm, initialize} from 'redux-form'
 import {showTabs, selectTab} from '../comum/tab/tabActions'
 
 const URL_BASE = 'http://localhost:3003/api'
+const VALORES_INICIAIS = {}
 
 export function pegaLista(){
     const request = axios.get(`${URL_BASE}/ciclosPagamento`)
@@ -19,12 +20,7 @@ export function create(values){
         axios.post(`${URL_BASE}/ciclosPagamento`, values)
             .then(resp =>{
                 toastr.success('Sucesso', 'Operação realizada com sucesso!')
-                dispatch([
-                    resetForm('cicloPagamentoForm'),
-                    pegaLista(),
-                    selectTab('tabList'),
-                    showTabs('tabList', 'tabCreate')
-                ])
+                dispatch(init())
             })
             .catch(e =>{
                 e.response.data.errors.forEach(error => toastr.error('Erro', error))
@@ -37,5 +33,14 @@ export function showUpdate(cicloPagamento){
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('cicloPagamentoForm', cicloPagamento)
+    ]
+}
+
+export function init(){
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        pegaLista(),
+        initialize('cicloPagamentoForm', VALORES_INICIAIS)
     ]
 }
--- aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/cicloPagamentoForm.jsx
+++ aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/cicloPagamentoForm.jsx
@@ -1,7 +1,10 @@
 import React, {Component} from "react"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
 import { reduxForm, Field } from "redux-form"
 
 import labelAndInput from "../comum/form/labelAndInput"
+import { init } from "./cicloPagamentoActions"
 
 class CicloPagamentoForm extends Component{
     render(){
@@ -15,10 +18,13 @@ class CicloPagamentoForm extends Component{
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="button" className="btn btn-default" onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )
     }
 }
 
-export default reduxForm({form: 'cicloPagamentoForm', destroyOnUnmount: false})(CicloPagamentoForm)
+CicloPagamentoForm = reduxForm({form: 'cicloPagamentoForm', destroyOnUnmount: false})(CicloPagamentoForm)
+const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
+export default connect(null, mapDispatchToProps)(CicloPagamentoForm)
