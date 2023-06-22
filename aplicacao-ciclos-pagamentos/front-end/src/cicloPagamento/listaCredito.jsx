ba2ab3c2b2ac38688e1f0550f9c177ad7f5a90e0
Victor Portugal <victorportugalnobrega@gmail.com>
2023-06-22 18:50:29 -0300

    Implementando a função de remoção de lista de crédito

--- aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/listaCredito.jsx
+++ aplicacao-ciclos-pagamentos/front-end/src/cicloPagamento/listaCredito.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
-import { Field, arrayInsert } from "redux-form"
+import { Field, arrayInsert, arrayRemove } from "redux-form"
 
 import Grid from '../comum/layout/grid'
 import Input from "../comum/form/input"
@@ -10,6 +10,11 @@ class ListaCredito extends Component{
     add(index, item = {}){
         if(!this.props.readOnly){
             this.props.arrayInsert('cicloPagamentoForm', 'creditos', index, item)
+        }
+    }
+    remove(index){
+        if(!this.props.readOnly && this.props.list.length > 1){
+            this.props.arrayRemove('cicloPagamentoForm', 'creditos', index)
         }
     }
     renderRows(){
@@ -21,6 +26,7 @@ class ListaCredito extends Component{
                 <td>
                     <button type="button" className="btn btn-success" onClick={() => this.add(index + 1)}><i className="fa fa-plus"></i></button>
                     <button type="button" className="btn btn-warning" onClick={() => this.add(index + 1, item)}><i className="fa fa-clone"></i></button>
+                    <button type="button" className="btn btn-danger" onClick={() => this.remove(index)}><i className="fa fa-trash-o"></i></button>
                 </td>
             </tr>
 
@@ -50,5 +56,5 @@ class ListaCredito extends Component{
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert, arrayRemove}, dispatch)
 export default connect(null, mapDispatchToProps)(ListaCredito)
