be4cf774c754cd00893aac1fccda2ae1dd2a4ff6
Victor Portugal <victorportugalnobrega@gmail.com>
2023-07-17 19:45:35 -0300

    Multíplos componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,5 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import BomDia from './components/BomDia'
+import { BoaTarde, BoaNoite } from './components/Multiplos'
 
-ReactDOM.render(<BomDia nome='Satoru Gojo' />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <BomDia nome='Satoru Gojo' />
+        <BoaTarde nome='Son Goku' />
+        <BoaNoite nome='Zaraki Kenpachi' />
+    </div>
+, document.getElementById('root'))
