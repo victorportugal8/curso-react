21dacf2eec7b294777d958487511bb30eb586914
Victor Portugal <victorportugalnobrega@gmail.com>
2023-07-21 17:49:07 -0300

    Componente de classe 2

--- react/exercicios/src/components/Saudacao.jsx
+++ react/exercicios/src/components/Saudacao.jsx
@@ -5,6 +5,10 @@ export default class Saudacao extends Component{
         tipo: this.props.tipo,
         nome: this.props.nome
     }
+    constructor(props){
+        super(props)
+        this.setTipo = this.setTipo.bind(this)
+    }
     setTipo(e){
         this.setState({tipo: e.target.value})
     }
@@ -17,8 +21,8 @@ export default class Saudacao extends Component{
         <div>
             <h1>{tipo} {nome}</h1>
             <hr />
-            <input type="text" placeholder='Tipo' value={tipo} onChange={e => this.setTipo(e)} />
-            <input type="text" placeholder='Nome' value={nome} onChange={e => this.setNome(e)} />
+            <input type="text" placeholder='Tipo' value={tipo} onChange={this.setTipo} />
+            <input type="text" placeholder='Nome' value={nome} onChange={e =>this.setNome(e)} />
         </div>
        )
     }
