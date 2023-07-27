04b38b9801f7c3295ca4c6ae0bbfe6c965ee133b
Victor Portugal <victorportugalnobrega@gmail.com>
2023-07-27 19:26:25 -0300

    Implementando Rotas

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -2,10 +2,11 @@ import './logo.css'
 import logo from '../../assets/images/logo.png'
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="logo">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img src={logo} alt="Logo" />
-        </a>
+        </Link>
     </aside>
