93462f3cef89ee0beb82c2f995a8eeb949877292
Victor Portugal <victorportugalnobrega@gmail.com>
2023-07-28 17:55:42 -0300

    Implementando o componente UserCrud

--- react/crud/frontend/src/components/home/Home.jsx
+++ react/crud/frontend/src/components/home/Home.jsx
@@ -6,5 +6,5 @@ export default props =>
     <Main icon="home" title="Início" subtitle="Projeto React - CRUD">
         <div className="display-4">Bem-vindo!</div>
         <hr />
-        <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React.</p>
+        <p className="mb-0">Aplicação para exemplificar a construção de um sistema de cadastro de usuários, desenvolvido em React.</p>
     </Main>
